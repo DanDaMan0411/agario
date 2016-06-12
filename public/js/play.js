d8f19e12cdd28a867c99dc30268abdb9a06a82a2
DanDaMan0411 <dan.kirichok@gmail.com>
2016-06-11 20:07:21 -0400

    Synced up food positions in all connected worlds. Food also respawns whenever eaten. Synced player positioning is not yet supported and will be worked on next.

--- public/js/play.js
+++ public/js/play.js
@@ -1,4 +1,10 @@
-var maxFoodAmount = 500
+var maxFoodAmount = 10
+var BLOB_ID;
+var foodPos;
+
+//This is for the blob
+var startDim = 100;
+
 var foodColors = [
 	"greenFood",
 	"purpleFood",
@@ -7,13 +13,65 @@ var foodColors = [
 	"redFood",
 ]
 
-var startDim = 100;
+var blobTypes = [
+	"uruguayBlob",
+]
+
+function getRandomInt(min, max){
+	return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+var playerBlobType = blobTypes[getRandomInt(0, blobTypes.length-1)]
+
+var socket = io();
+
+//Gives the blob the socket id it has
+socket.on("assignId", function(newPlayerId){
+	BLOB_ID = newPlayerId;
+})
+
+//This runs when player first connects, it initializes all the food positions
+socket.on("makeFood", function(foodPositions){
+	foodPos = foodPositions;
+})
+
+//These are queues for when food nneds to be either generated or removed from clients world
+var removeQueue = []
+var addQueue = []
+
+socket.on("removeFood", function(eatenPosition){
+	removeQueue.push(eatenPosition);
+})
+
+socket.on("addFood", function(newFoodPosition){
+	addQueue.push(newFoodPosition);
+})
 
 var playState = {
 	getRandomInt: function(min, max) {
 		return Math.floor(Math.random() * (max - min + 1)) + min;
 	},
-
+	
+	//This runs whenever one of the elements in blobInfo has been changed on the client
+	updateBlob: function(){
+		var blobInfo = this.makeBlobInfo();
+		
+		socket.emit("updateBlob", blobInfo);
+	},
+	
+	makeBlobInfo: function(){
+		//All of the blobs important information for storage on main server		
+		var blobInfo = {
+			blobType: playerBlobType,
+			x: blob.x,
+			y: blob.y,
+			mass: blob.body.data.mass,
+			width: blob.width,
+			height: blob.height,
+		}
+		return blobInfo
+	},
+	
 	initFood: function(){
 		foods = game.add.group();
 		
@@ -21,22 +79,25 @@ var playState = {
 		foods.physicsBodyType = true;
 		foods.physicsBodyType = Phaser.Physics.P2JS;
 		
-		for (var i = 0; i < maxFoodAmount; i ++){
-			
-			
-			var food = foods.create(this.getRandomInt(0, game.world.width), this.getRandomInt(0, game.world.height), foodColors[this.getRandomInt(0, foodColors.length-1)]);
-			food.body.setCircle(10);
-			
-			food.body.velocity = (0, 0)
-			
-			food.body.setCollisionGroup(foodCollisionGroup);
-			
-			food.body.collides([foodCollisionGroup, playerCollisionGroup])
+		for (var i = 0; i < maxFoodAmount; i ++){			
+			if (foodPos[i] != undefined){
+				var food = foods.create(foodPos[i][0], foodPos[i][1], foodColors[this.getRandomInt(0, foodColors.length-1)]);
+				
+				food.id = i;
+				
+				food.body.setCircle(10);
+				
+				food.body.velocity = (0, 0)
+				
+				food.body.setCollisionGroup(foodCollisionGroup);
+				
+				food.body.collides([foodCollisionGroup, playerCollisionGroup])
+			}
 		}
 	},
 	
-	initPlayer: function(posX, posY){
-		blob = game.add.sprite(posX, posY, 'uruguayBlob');
+	initPlayer: function(xPos, yPos, blobType){
+		blob = game.add.sprite(xPos, yPos, blobType);
 		
 		game.physics.p2.enable(blob, false);
 		
@@ -47,31 +108,34 @@ var playState = {
 		blob.body.setCollisionGroup(playerCollisionGroup);
 		
 		blob.body.collides(foodCollisionGroup, this.eatFood, this);
-		blob.body.collides(playerCollisionGroup, this.hitPlayer, this);
 		
 		game.camera.follow(blob);
 		
 		//Start Mass
 		blob.body.data.mass = 25;
 		
-		blob.name = "player"
+		blob.name = "blob"
 		
 		cursors = game.input.keyboard.createCursorKeys();
-	},
-	
-	hitPlayer: function(playerBody, enemyBody){
-		//enemyBody.sprite.body.clearCollision(true);
+		
+		var blobInfo = this.makeBlobInfo();
+		
+		socket.emit("newPlayer", blobInfo)
 	},
 	
 	eatFood: function(blobBody, foodBody){		
 		blob.body.data.mass ++;
 		
-		console.log(blob.body.data.mass);
+		var foodBodyPosition = foodBody.sprite.id
 		
+		socket.emit("foodEaten", foodBodyPosition)	
+				
 		foodBody.sprite.kill();
 			
 		blobBody.sprite.height ++
 		blobBody.sprite.width ++
+		
+		this.updateBlob();
 		
 		blobBody.sprite.body.setCircle(blobBody.sprite.width/2);
 		blobBody.sprite.body.setCollisionGroup(playerCollisionGroup);
@@ -85,7 +149,7 @@ var playState = {
 		
 		game.physics.p2.restitution = 0.8;
 		
-		game.world.setBounds(0, 0, 3000, 3000)
+		game.world.setBounds(0, 0, 500, 500)
 		
 		//This repeats the grid background to fit the whole bounds
 		game.add.tileSprite(0, 0, game.world.bounds.height, game.world.bounds.width, 'grid')
@@ -96,41 +160,82 @@ var playState = {
 		foodCollisionGroup = game.physics.p2.createCollisionGroup();
 		
 		game.physics.p2.updateBoundsCollisionGroup();
-
+		
 		this.initFood();
 		
-		this.initPlayer(game.world.centerX, game.world.centerY);
+		this.initPlayer(this.getRandomInt(startDim, game.world.width - startDim), this.getRandomInt(startDim, game.world.width - startDim), playerBlobType);
+		
+		socket.on("playerAdded", function(newPlayerInfo){
+			//This function should be out on the top layer but it doesn't work out there. Try to get a fix at some point
+			function spawnEnemy(xPos, yPos, blobType, mass, width, height){
+				var enemy = game.add.sprite(xPos, yPos, blobType);
+				
+				game.physics.p2.enable(enemy, false);
+				
+				enemy.width = width;
+				enemy.height = height;
+				
+				enemy.body.setCircle(enemy.width/2);
+				
+				enemy.body.fixedRotation = true;
+				
+				enemy.body.setCollisionGroup(playerCollisionGroup);
+				
+				enemy.body.collides(foodCollisionGroup, this.eatFood, this);
+				enemy.body.collides(playerCollisionGroup, this.hitPlayer, this);
+						
+				//Start Mass
+				enemy.body.data.mass = mass;
+				
+				enemy.name = "enemy"
+			}
+			
+			spawnEnemy(newPlayerInfo.x, newPlayerInfo.y, newPlayerInfo.blobType, newPlayerInfo.mass, newPlayerInfo.width, newPlayerInfo.height)
+			console.log("enemy spawned")
+		})
 	},
 	
 	checkSprite: function(body1, body2) {
-		//  To explain - the post broadphase event has collected together all potential collision pairs in the world
-		//  It doesn't mean they WILL collide, just that they might do.
-
-		//  This callback is sent each collision pair of bodies. It's up to you how you compare them.
-		//  If you return true then the pair will carry on into the narrow phase, potentially colliding.
-		//  If you return false they will be removed from the narrow phase check all together.
-
-		//  In this simple example if one of the bodies is our space ship, 
-		//  and the other body is the green pepper sprite (frame ID 4) then we DON'T allow the collision to happen.
-		//  Usually you would use a collision mask for something this simple, but it demonstates use.
-		
 		if ((body1.sprite.name == 'enemy' && body2.sprite.name == 'player') || (body2.sprite.name == 'player' && body1.sprite.name == "enemy")){			
 			return false;
-		}else{
-			console.log(body1.sprite.name)	
-			console.log(body2.sprite.name)	
 		}
 
 		return true;
 	},
 	
-	update: function(){	
+	addFood: function(position){
+		var food = foods.create(foodPos[position][0], foodPos[position][1], foodColors[this.getRandomInt(0, foodColors.length-1)]);
+		
+		food.id = position;
+		
+		food.body.setCircle(10);
+		
+		food.body.velocity = (0, 0)
+		
+		food.body.setCollisionGroup(foodCollisionGroup);
+		
+		food.body.collides([foodCollisionGroup, playerCollisionGroup])
+		
+		return food
+	},
+	
+	update: function(){
 		blob.body.setZeroVelocity();
 		
 		if (Phaser.Rectangle.contains(blob.body, game.input.x, game.input.y)){
 			blob.body.velocity.setTo(0, 0);
 		}else{
 			game.physics.arcade.moveToPointer(blob, 200);
+		}
+						
+		if (removeQueue.length > 0){
+			foods.children[removeQueue[0]].kill();
+			removeQueue.shift();
+		}
+		
+		if (addQueue.length > 0){
+			foods.children[addQueue[0]] = this.addFood(addQueue[0]);
+			addQueue.shift();
 		}
 	},
 }
--- server.js
+++ server.js
@@ -7,10 +7,87 @@ var path = require('path')
 var http = require('http').Server(app)
 var io = require('socket.io')(http)
 
+function getRandomInt(min, max){
+	return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(req, res){
-	res.sendFile(__dirname + '/views/index.html')
+	res.sendFile(__dirname + '/views/index.html');
+})
+
+gameBlobs = {};
+
+var maxFoodAmount = 10;
+var worldDimension = 500;
+
+//Sets the foodPositions for the world. Produces the max amount of food for the world.
+function makeFoodPositions(){
+	foodList = []
+	for (var i = 0; i < maxFoodAmount; i++){
+		var xPos = getRandomInt(0, worldDimension)
+		var yPos = getRandomInt(0, worldDimension)
+		
+		var position = [xPos, yPos]
+		
+		foodList.push(position)
+	}
+	return foodList
+}
+
+//This returns one coordinate for food to be made
+function generateNewFood(){
+	var xPos = getRandomInt(0, worldDimension)
+	var yPos = getRandomInt(0, worldDimension)
+	
+	var position = [xPos, yPos]
+	
+	return position
+}
+
+//This is the variable that contains the food positions for food for all players
+var gameFoodPositions = makeFoodPositions()
+
+io.on("connection", function(socket){
+	console.log("Connection");
+	
+	console.log(gameFoodPositions.length + "\n")
+		
+	socket.emit("makeFood", gameFoodPositions);
+	
+	socket.on("newPlayer", function(blobInfo){
+		makeFoodPositions();
+		socket.emit("assignId", socket.id);
+		
+		gameBlobs[socket.id] = blobInfo;
+				
+		socket.broadcast.emit("playerAdded", blobInfo)
+	});
+	
+	socket.on("foodEaten", function(eatenPosition){
+		console.log("Eaten Position: " + eatenPosition)
+		
+		gameFoodPositions[eatenPosition] = generateNewFood();
+		console.log(gameFoodPositions.length)
+		socket.broadcast.emit("removeFood", eatenPosition);
+		
+		//This updates the food list held by all clients
+		io.sockets.emit("makeFood", gameFoodPositions);
+		
+		//This tells clients to make the new food that replaced the eaten one
+		io.sockets.emit("addFood", eatenPosition);
+	})
+	
+	socket.on("updateBlob", function(newInfo){
+		gameBlobs[socket.id] = newInfo
+	});
+	
+	socket.on("disconnect", function(){
+		console.log(socket.id + " Disconnected");
+		
+		delete gameBlobs[socket.id]
+	});
 })
 
 http.listen(4000, function(){
